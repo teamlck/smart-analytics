74d2c41c1e5bb6d9a524543983dc2f6496af9b05
norux <andrew@norux.me>
2018-09-16 20:41:11 +0900

    [WIP] Collect touch events

--- src/collector/events/touch.js
+++ src/collector/events/touch.js
@@ -4,29 +4,54 @@ export class TouchEventCollector extends Collector {
   constructor (store) {
     super(store)
 
-    this.cache = []
+    this.cache = {}
   }
 
   registerEvent () {
-    this.on('touchstart', this.touchStart)
-    this.on('touchmove', this.touchMove)
-    this.on('touchend', this.touchEnd)
-    this.on('touchcancel', this.touchEnd)
+    this.on('touchstart', e => this.touchStart(e))
+    this.on('touchmove', e => this.touchMove(e))
+    this.on('touchend', e => this.touchEnd(e))
+    this.on('touchcancel', e => this.touchEnd(e))
   }
 
-  touchStart (ev) {
-    this.cache.push(ev)
+  touchStart (e) {
+    /*
+      Touch
+      clientX : 490.0255126953125
+      clientY : 981.4205932617188
+      force : 1
+      identifier : 0
+      pageX : 490.0255126953125
+      pageY : 981.4205932617188
+      radiusX : 25.044443130493164
+      radiusY : 25.044443130493164
+      rotationAngle : 0
+      screenX : 597.01171875
+      screenY : 637.65234375
+    */
+    this.cache.strat = Date.now()
+
+    for (let i = 0 ; i < e.touches.length; i++) {
+    }
+
+    this.cache.touches = e.touches
+    this.cache.target = e.target
 
     // TODO Not yet
   }
 
-  touchMove (ev) {
+  touchMove (e) {
     // TODO Not yet
+
+    this.cache.touches.push(...e.touches)
+    this.cache.target = e.target
   }
 
-  touchEnd (ev) {
+  touchEnd (e) {
     // TODO Not yet
 
-    super.save(ev)
+    console.log(e)
+    this.save(this.cache)
+    this.cache = {}
   }
 }
