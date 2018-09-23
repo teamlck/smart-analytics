e01ea98fb87f56831a53e9e8619bf952c3bd2b30
norux <andrew@norux.me>
2018-09-23 12:08:56 +0900

    Remove useless constructor (Fix travis failed)

--- src/collector/events/resize.js
+++ src/collector/events/resize.js
@@ -1,10 +1,6 @@
 import { Collector } from '../collector'
 
 export class ResizeEventCollector extends Collector {
-  constructor (store) {
-    super(store)
-  }
-
   registerEvent () {
     this.on('resize', e => this.resize(e))
   }
